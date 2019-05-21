03a5ee8ed67964ef4f97f3cd0537edbdae8fc638
bunio <kicjow.ketrab@gmail.com>
2019-05-21 19:03:36 +0100

    processes all the file with IRL (high model 4 states)

--- src/inverse_reinforcement_learning/main.py
+++ src/inverse_reinforcement_learning/main.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import settings
 import os
@@ -8,35 +7,37 @@ from Mdp.mdp_utils import MdpUtils
 from Mdp.transition_counting_translator import TransitionCountingTranslator
 from inverse_reinforcement_learning.compare_processor import CompareProcessor
 from inverse_reinforcement_learning.irl_processor import IrlProcessor
+from data_const import UsableConversationConstants as const_uc
 
 if __name__ == "__main__":
-
-    FILE_NAME = "human_readable_conversation_9.json"
-    FILE_TO_READ = os.path.join(settings.HUMAN_READABLE_FOLDER_PATH, FILE_NAME)
+    HUMAN_READABLE_FOLDER_PATH = settings.HUMAN_READABLE_FOLDER_PATH
     METADATA_PATH = settings.READABLE_METADATA_FILE_PATH
 
     with open(METADATA_PATH, "r") as metadata_file:
+
         metadata_json = json.loads(metadata_file.read())
 
-        transition_counting_array = np.load(
-            os.path.join(
-                settings.MY_DATA_FOLDER_PATH, "transition_counting_results.npy"
-            )
-        )
-        translator = TransitionCountingTranslator(transition_counting_array)
+        for filename in os.listdir(HUMAN_READABLE_FOLDER_PATH):
+            full_file_name = os.path.join(HUMAN_READABLE_FOLDER_PATH, filename)
 
-        with open(FILE_TO_READ, "r") as conversation_file:
 
-            this_file_metadata = metadata_json[FILE_NAME]
-            conv_json = json.loads(conversation_file.read())
+            with open(full_file_name, "r") as conversation_file:
 
-            mdp_graph = MdpUtils.get_at_high_mdp_model()
+                this_file_metadata = metadata_json[full_file_name]
+                conv_json = json.loads(conversation_file.read())
 
-            processor = IrlProcessor()
-            irl_result = processor.process(conv_json,mdp_graph,this_file_metadata,FILE_NAME)
+                mdp_graph = MdpUtils.get_at_high_mdp_model()
 
-            compare_processor = CompareProcessor()
-            compare_processor.compare(irl_result,FILE_NAME,conv_json,this_file_metadata,12,True)
+                processor = IrlProcessor()
+                irl_result = processor.process(
+                    conv_json, mdp_graph, this_file_metadata, full_file_name
+                )
 
+                compare_processor = CompareProcessor()
+                compare_processor.compare(
+                    irl_result, full_file_name, conv_json, this_file_metadata, 12, True
+                )
+
+                #TODO might do something with irl_result later ¯\_(ツ)_/¯
 
         debug = 5
