520b3231e774296d417ce2fdd88c56134e446d75
bunio <kicjow.ketrab@gmail.com>
2019-06-17 22:26:03 +0100

    manual tester updated

--- src/inverse_reinforcement_learning/aaaaa_test.py
+++ src/inverse_reinforcement_learning/aaaaa_test.py
@@ -12,21 +12,19 @@ t_file_path = "C:\\Users\\kicjo\\Documents\\PythonProjects\\DataPreprocessing-Ma
 
 ts = np.load(t_file_path)
 
-min_t = np.argmin(ts)
+policies = np.load(policies_file_path)
 
-
-policy = np.load(policies_file_path)[5]
-
-settings = Settings(MAX_CONTINUOUS_TIME_SEC=10.0,
-                    DISCOUNT_FACTOR=0.999999,
-                    POLICY_THETA=0.01,
-                    IRL_SOLVER_EPSILON=0.05,
-                    Q_ITERATIONS=100,
-                    Q_ALPHA=0.4,
-                    Q_EPSILON=0.2)
+settings = Settings(
+    MAX_CONTINUOUS_TIME_SEC=10.0,
+    DISCOUNT_FACTOR=0.999999,
+    POLICY_THETA=0.01,
+    IRL_SOLVER_EPSILON=0.05,
+    Q_ITERATIONS=100,
+    Q_ALPHA=0.4,
+    Q_EPSILON=0.2,
+)
 
 model = MdpUtils.get_at_high_mdp_model(settings)
-
 
 
 METADATA_PATH = settings.READABLE_METADATA_FILE_PATH
@@ -36,13 +34,19 @@ with open(METADATA_PATH, "r") as metadata_file:
 
     metadata = metadata_json["human_readable_conversation_16.json"]
 
-    player = HighPolicyPlayer(metadata,model,0.05)
+    player = HighPolicyPlayer(metadata, model, 0.05)
 
-    conversation = player.play_policy(policy,1200)
+    conversations = [player.play_policy(policy,16000) for policy in policies]
 
     comparer = ConversationComparer()
-    comparer.compare_and_save_plots("16_with_made_up",conversation,conversation,1,metadata,False,model.Ca.shape,settings)
-
-
-
-
+    comparer.compare_and_save_plots(
+        "16_with_made_up",
+        conversations[0],
+        conversations,
+        ts,
+        1,
+        metadata,
+        False,
+        model.Ca.shape,
+        settings,
+    )
